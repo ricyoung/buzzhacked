06c9c88dbed32feef39fd051ebd977910378479a
ric <ricyoung@gmail.com>
2016-10-15 20:11:20 -0700

    data update and base working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,36 @@
+require 'net/http'
+require 'open-uri'
+
 class WelcomeController < ApplicationController
+
+
+
+
+  def fetch
+    email='ricyoung@gmail.com'
+    inurl = 'https://haveibeenpwned.com/api/v2/breachedaccount/' + email
+    url = URI(inurl)
+    @res =  Net::HTTP.get(url)
+    response = JSON.parse(@res)
+    response.each do | p|
+      pwn=Pwnd.new
+      pwn.title = p['Title']
+      pwn.name = p['Name']
+      pwn.domain = p['Domain']
+      pwn.breachdate = p['BreachDate']
+      pwn.addeddate = p['AddedDate']
+      pwn.pwncount = p['PwnCount']
+      pwn.description = p['Description']
+      pwn.dataclass = p['DataClasses']
+      pwn.isverified = p['IsVerified']
+      pwn.issensitive = p['IsSensitive']
+      pwn.isretired = p['IsRetired']
+      pwn.email = email
+      pwn.save
+    end
+
+ 
+
+  end
+
 end
