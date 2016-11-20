a2d801d3ca63c64b14f8710ab1c67f2d9b5f830f
ric <ricyoung@gmail.com>
2016-11-19 17:47:25 -0800

    social fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,9 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require jquery.prettySocial.min
+//= require jquery.prettySocial
 //= require_tree .
-
-//= require foundation
-
 $(document).foundation()
 $(function(){ $(document).foundation(); });
-$('.prettySocial').prettySocial();
+
 
