6f123adce19a1a0c719218340508bc32b532cc18
ric <ricyoung@gmail.com>
2016-11-19 18:15:56 -0800

    title, removed extra stuff, and other social posting issues

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+=begin
 Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( jquery.prettySocial.min.js )
+Rails.application.config.assets.precompile += %w( jquery.prettySocial.js )
+=end
