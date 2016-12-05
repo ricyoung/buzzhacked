b86bfdfa1dec8c4054ad45784ae323b08397683d
ric <ricyoung@gmail.com>
2016-12-04 18:20:02 -0800

    removed stuff from FAQ

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  # root 'welcome#index'
-  root 'pages#faq'
+  root 'welcome#index'
+  #root 'pages#faq'
   match 'fetch', to: 'welcome#fetch', via: :all
 
   get 'about', to: 'pages#about'
